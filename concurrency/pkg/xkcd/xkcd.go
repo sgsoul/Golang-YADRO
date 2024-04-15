d5934317ea0f8f8d66cd30d8bbbd5640cfcfc151
sgsoul <irina.loskutova@gmail.com>
2024-04-16 00:18:38 +0300

    go fmt

--- concurrency/pkg/xkcd/xkcd.go
+++ concurrency/pkg/xkcd/xkcd.go
@@ -9,8 +9,8 @@ import (
 	"sync"
 
 	"github.com/sgsoul/pkg/database"
-	"github.com/sgsoul/pkg/words"
 	"github.com/sgsoul/pkg/style"
+	"github.com/sgsoul/pkg/words"
 )
 
 type Client struct {
@@ -18,8 +18,8 @@ type Client struct {
 }
 
 type ComicWithID struct {
-    ID    int
-    Comic database.Comic
+	ID    int
+	Comic database.Comic
 }
 
 func NewClient(url string) *Client {
@@ -74,66 +74,66 @@ func (c *Client) retrieveComic(num int) (database.Comic, error) {
 }
 
 func (c *Client) RunWorkers(workers int, dbFile string) {
-    var (
-        wg           sync.WaitGroup
-        idChannel    = make(chan int)          // канал для передачи id комиксов
-        comicsChannel = make(chan ComicWithID) // канал для передачи загруженных комиксов
-    )
+	var (
+		wg            sync.WaitGroup
+		idChannel     = make(chan int)         // канал для передачи id комиксов
+		comicsChannel = make(chan ComicWithID) // канал для передачи загруженных комиксов
+	)
 
 	loadingIndicator := style.StartLoadingIndicator()
 
-    // receiver
-    go func() {
-        for i := 1; ; i++ {
-            if _, err := os.Stat(dbFile); os.IsNotExist(err) {
-                if _, err := os.Create(dbFile); err != nil {
-                    fmt.Println("error creating the database:", err)
-                    return
-                }
-            }
-            comicsMap, err := database.LoadComicsFromFile(dbFile)
-            if err != nil {
-                fmt.Println("error loading comics from the database:", err)
-                return
-            }
+	// receiver
+	go func() {
+		for i := 1; ; i++ {
+			if _, err := os.Stat(dbFile); os.IsNotExist(err) {
+				if _, err := os.Create(dbFile); err != nil {
+					fmt.Println("error creating the database:", err)
+					return
+				}
+			}
+			comicsMap, err := database.LoadComicsFromFile(dbFile)
+			if err != nil {
+				fmt.Println("error loading comics from the database:", err)
+				return
+			}
 			//проверка что комикс есть в дб
-            if _, ok := comicsMap[fmt.Sprintf("%d", i)]; !ok && i != 404 {
-                idChannel <- i // в канал загрузки
-            }
-        }
-    }()
+			if _, ok := comicsMap[fmt.Sprintf("%d", i)]; !ok && i != 404 {
+				idChannel <- i // в канал загрузки
+			}
+		}
+	}()
 
-    // workers
-    for i := 0; i < workers; i++ {
-        wg.Add(1)
-        go func() {
-            defer wg.Done()
-            for id := range idChannel {
-                comic, err := c.retrieveComic(id)
-                if err != nil {
-                    // check the next one
-                    _, err := c.retrieveComic(id + 1)
-                    if err != nil {
-                        // exit if double 404
-                        return
-                    }
-                    continue
-                }
-                comicsChannel <- ComicWithID{ID: id, Comic: comic}
-            }
-        }()
-    }
+	// workers
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for id := range idChannel {
+				comic, err := c.retrieveComic(id)
+				if err != nil {
+					// check the next one
+					_, err := c.retrieveComic(id + 1)
+					if err != nil {
+						// exit if double 404
+						return
+					}
+					continue
+				}
+				comicsChannel <- ComicWithID{ID: id, Comic: comic}
+			}
+		}()
+	}
 
-    // collector
-    go func() {
-        defer close(comicsChannel) // закрываем канал при функции
-        for comicWithID := range comicsChannel {
-            database.SaveComicsToDatabase(dbFile, comicWithID.Comic, comicWithID.ID)
-        }
-    }()
+	// collector
+	go func() {
+		defer close(comicsChannel) // закрываем канал при функции
+		for comicWithID := range comicsChannel {
+			database.SaveComicsToDatabase(dbFile, comicWithID.Comic, comicWithID.ID)
+		}
+	}()
 
-    // wait 4 all worker goroutines to finish
-    wg.Wait()
+	// wait 4 all worker goroutines to finish
+	wg.Wait()
 	style.StopLoadingIndicator(loadingIndicator)
-    fmt.Println("\nall comics saved to", dbFile)
+	fmt.Println("\nall comics saved to", dbFile)
 }
