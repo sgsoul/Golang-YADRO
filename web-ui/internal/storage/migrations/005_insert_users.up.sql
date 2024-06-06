08f4cf379d1cc9adf1e98150e840c2ed5c3c620e
sgsoul <irina.loskutova@gmail.com>
2024-06-06 15:35:17 +0300

    9th task

---
+++ web-ui/internal/storage/migrations/005_insert_users.up.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO users (username, password, role) 
+VALUES 
+    ('admin', '$2a$10$eKAvhwoJTIbFDLQwCYiZW.b25GuTv3isO6vEn66n5rpmbPMV1gfta', 'admin'),
+    ('user1', '$2a$10$YYUUgKpR8ERRdBLGlFZtz.Z6v6kJo4ndsoCedpXYVVUQBFtqXBquK', 'user'),
+    ('user2', '$2a$10$YYUUgKpR8ERRdBLGlFZtz.Z6v6kJo4ndsoCedpXYVVUQBFtqXBquK', 'user');
