7789b8523a718e28eb4754dc06611182d9c6c435
sgsoul <93263659+sgsoul@users.noreply.github.com>
2024-06-06 15:23:26 +0300

    Delete access directory

--- access/internal/core/database/migrations/005_insert_users.up.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (username, password, role) 
-VALUES 
-    ('admin', '$2a$10$eKAvhwoJTIbFDLQwCYiZW.b25GuTv3isO6vEn66n5rpmbPMV1gfta', 'admin'),
-    ('user1', '$2a$10$YYUUgKpR8ERRdBLGlFZtz.Z6v6kJo4ndsoCedpXYVVUQBFtqXBquK', 'user'),
-    ('user2', '$2a$10$YYUUgKpR8ERRdBLGlFZtz.Z6v6kJo4ndsoCedpXYVVUQBFtqXBquK', 'user');
